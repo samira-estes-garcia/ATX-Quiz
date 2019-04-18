a15bf7210c789ef1e7ecee2cd9e954dced7037fb
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-04-18 15:50:03 -0500

    added style and responsive

--- datastore.js
+++ datastore.js
@@ -3,71 +3,73 @@ const STORE = [
     {
         question: "Which local coffee shop stays open 24/7?",
         answers: ["Halcyon", "Starbucks", "Bennu", "Houndstooth"],
-        correctAnswer: "Bennu",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
+        correctAnswer: 2,
+        icon: "https://assets.simpleviewinc.com/simpleview/image/fetch/q_90/https://assets.simpleviewinc.com/simpleview/image/upload/crm/austin/Bennu-MLK-Sign--d7d7720ac4cc8e3_d7d7740e-b1b5-1308-d68267149af19e87.jpg",
+        alt: "Image displaying storefront of Bennu coffe shop"
     },
     {
         question: "Which bridge can you visit to see the bats?",
         answers: ["Lamar Bridge", "Congress Ave. Bridge", "360 Bridge", "Pennybacker Bridge"],
-        correctAnswer: "Congress Ave. Bridge"
-        icon: "",
-        alt: ""
-    },
-    {
-        question: "What iconic street borders the University of Texas at Austin?",
-        answers: ["Guadalupe St.", "51st St.", "Congress Ave.", "Cesar Chavez St."],
-        correctAnswer: "Guadalupe St."
-        icon: "",
-        alt: ""
+        correctAnswer: 1,
+        icon: "https://photos.smugmug.com/Low-Resolution/Congress-Avenue-Bridge-Bats/i-vqVZXJV/5/56d40a35/L/MDT_130825_US3_2174-Edit-Edit-L.jpg",
+        alt: "Image displaying the Congress Ave. Bridge at sunset with bats flying out from underneath it"
     }
-    {
-        question: "What is the tallest building in ATX?",
-        answers: ["The Fairmont", "Frost Tower", "The Bowie", "The Austonian"],
-        correctAnswer: "The Austonian",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
-    },
-    {
-        question: "Which local shop is known for selling costumes?",
-        answers: ["Party City", "Lucy in Disguise with Diamonds", "Cathedral of Junk", "Austin Art Garage"],
-        correctAnswer: "Bennu",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
-    },
-    {
-        question: "Which of the following bars is located on West 6th St.?",
-        answers: ["Maggie Mae's", "Whisler's", "Shangri-La", "Violet Crown Social Club"],
-        correctAnswer: "Maggie Mae's",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
-    },
-    {
-        question: "What is the mascot of the University of Texas?",
-        answers: ["Bears", "Roadrunners", "Raiders", "Longhorns"],
-        correctAnswer: "Longhorns",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
-    },
-    {
-        question: "What is the name of the famous yearly music festival that is usually held in the Fall?",
-        answers: ["SXSW", "Austin City Limits (ACL)", "F1", "Waterloo Music Festival"],
-        correctAnswer: "Austin City Limits (ACL)",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
-    },
-    {
-        question: "What river cuts through downtown Austin?",
-        answers: ["Rio Grande River", "Guadalupe River", "Colorado River", "San Antonio River"],
-        correctAnswer: "Colorado River",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
-    },
-    {
-        question: "Where can LGBTQ+ and other similarly identified folks go to party downtown?",
-        answers: ["West 6th", "The Warehouse District", "Rainey St.", "East 6th"],
-        correctAnswer: "The Warehouse District",
-        icon: "",
-        alt: "Image displaying storefront of Bennu coffeshop"
-    }
-];
+]
+// ,
+//     {
+//         question: "What iconic street borders the University of Texas at Austin?",
+//         answers: ["Guadalupe St.", "51st St.", "Congress Ave.", "Cesar Chavez St."],
+//         correctAnswer: 0,
+//         icon: "https://archinect.imgix.net/uploads/0c/0cjhxiy56mecfl90.jpg?fit=crop&auto=compress%2Cformat&w=650",
+//         alt: "Image displaying students crossing Guadalupe St. to get to campus"
+//     },
+//     {
+//         question: "What is the tallest building in ATX?",
+//         answers: ["The Fairmont", "Frost Tower", "The Bowie", "The Austonian"],
+//         correctAnswer: 3,
+//         icon: "https://b386363e680359b5cc19-97ec1140354919029c7985d2568f0e82.ssl.cf1.rackcdn.com/assets/uploads/luxury_buildings/luxury_pic/photo/29082/optimized_3656213ef7c50fd6b3ad5d60092db4fc.jpg",
+//         alt: "Image displaying the Austonian"
+//     },
+//     {
+//         question: "Which local shop is known for selling costumes?",
+//         answers: ["Party City", "Lucy in Disguise with Diamonds", "Cathedral of Junk", "Austin Art Garage"],
+//         correctAnswer: 1,
+//         icon: "https://res.cloudinary.com/dostuff-media/image/upload//c_fill,g_faces,h_630,w_1200/v1476832317/venue-391226.jpg",
+//         alt: "Image displaying storefront of Lucy in Disguise with Diamonds"
+//     },
+//     {
+//         question: "Which of the following bars is located on West 6th St.?",
+//         answers: ["Maggie Mae's", "Whisler's", "Shangri-La", "Violet Crown Social Club"],
+//         correctAnswer: 0,
+//         icon: "https://i.imgur.com/n4gxPnA.png",
+//         alt: "Image displaying the front of Maggie Mae's at night"
+//     },
+//     {
+//         question: "What is the mascot of the University of Texas?",
+//         answers: ["Bears", "Roadrunners", "Raiders", "Longhorns"],
+//         correctAnswer: 3,
+//         icon: "https://snarkburbs.files.wordpress.com/2017/12/f63530eee9e5ca65d2ad3ca66e1ad661.jpg",
+//         alt: "Image displaying a burnt orange and white logo with a longhorn"
+//     },
+//     {
+//         question: "What is the name of the famous yearly music festival that is usually held in the Fall?",
+//         answers: ["SXSW", "Austin City Limits (ACL)", "F1", "Waterloo Music Festival"],
+//         correctAnswer: 1,
+//         icon: "https://cdn.vox-cdn.com/thumbor/L-hW1itF_ylCqcgEGE3tB0h3hK0=/0x0:4000x2670/1200x675/filters:focal(1680x1015:2320x1655)/cdn.vox-cdn.com/uploads/chorus_image/image/61638003/Marketing_Beauty_by_Katrina_Barber_16807.0.0.0.0.jpg",
+//         alt: "Image displaying a crowd of people in a park with the ACL logo"
+//     },
+//     {
+//         question: "What river cuts through downtown Austin?",
+//         answers: ["Rio Grande River", "Guadalupe River", "Colorado River", "San Antonio River"],
+//         correctAnswer: 2,
+//         icon: "https://i.imgur.com/RpRjg0X.png",
+//         alt: "Image displaying the Colorado River and the Austin skyline"
+//     },
+//     {
+//         question: "Where can LGBTQ+ and other similarly identified folks go to party downtown?",
+//         answers: ["West 6th", "The Warehouse District", "Rainey St.", "East 6th"],
+//         correctAnswer: 1,
+//         icon: "https://i.imgur.com/oegbjhI.png",
+//         alt: "Image displaying Queer bars in the Warehouse District"
+//     }
+// ];
--- index.js
+++ index.js
@@ -1,43 +1,144 @@
 console.log("is working");
 
 //Intro When button #start is pressed show question 
-$('#start').on('click', () => {
-    $('#question').css("display", "block");
-    $('#answers').css("display","block");
-    $('#intro').css("display", "none");
-});
-
-let count = 0; //tells us which question to render
-let score = 0; //tells the score out of 10
-
-
-
-//Question 
-//Need an array: objects, first object is a question number, plus question content, all choices, 
-const questionArray = [
-    {
-        question: "what is this color",
-        answers: ["red", "blue", "green", "orange"],
-        correctAnswer: "blue"
-    },
-    {
-        question: "what is this color",
-        answers: ["red", "blue", "green", "orange"],
-        correctAnswer: "green"
-    },
-    {
-        question: "what is this color",
-        answers: ["red", "blue", "green", "orange"],
-        correctAnswer: "red"
-    }
-];
-
 //Click on start, displays question and answer choices
+function startQuiz () {
+    $('#start').on('click', () => {
+        $('.score-container').css("display", "block");
+        $('#intro').css("display", "none");
+        $('.quiz-container').css("display", "block");
+        $('.questionNumber').text(1);
+        displayQuestion();
+    });
+}
 
-//When you select a choice and click the next button > will tell you whether the answer is right or not
-    //If its right, say you got it right and advance to the next question, increase the score
-    //if its not right, inform the user they got it wrong, tell them the right answer, and advance to next question
+let questionNumber = 0; //tells us which question to render
+let score = 0; //tells the score out of 10
+$('.total-questions').text(STORE.length);
 
-//When you finish all the questions, it would display your score
-//end = questionArray.length, 
-//If it's the last question, show the finish page, else render the next question
+//create the question to be displayed
+function createQuestion () {
+    const currentQuestion = STORE[questionNumber];
+    let resultHTML = "";
+    resultHTML += `<div class="question-item">`;
+    resultHTML += `<h1>${currentQuestion.question}</h1>`;
+    resultHTML += `<form> <fieldset>`
+    for (let i = 0; i < currentQuestion.answers.length; i++) 
+    {
+        resultHTML += `<div class="answer">
+        <input type="radio" name="answer" value="${i}" required/> ${currentQuestion.answers[i]} 
+        </div>`
+    }
+    resultHTML += `<button type="submit" class="submit">SUBMIT</button>`;
+    resultHTML += `</fieldset> </form>`;
+    resultHTML += `</div>`;
+    return resultHTML;
+}
+
+$('body').on('submit', 'form', (event) => {
+    event.preventDefault();
+    console.log('button is working');
+    incrementScore();
+})
+
+$('.continue').on('click', (event) => {
+    $('.quiz-container').css('display', 'block');
+    $('.quiz-feedback').css('display', 'none');
+    incrementQuestionNum();
+    if (questionNumber >= STORE.length) {
+    //display final score on end screen
+        displayFinish();
+        $('.questionNumber').text(STORE.length);
+    } else {
+        displayQuestion()
+    }
+})
+
+//display quiz in quiz container
+function displayQuestion() {
+    $('.quiz-container').html(createQuestion());
+}
+
+
+//increment questionNumber
+function incrementQuestionNum () {
+    questionNumber++;
+    $('.questionNumber').text(questionNumber+1);
+};
+
+
+//finish quiz screen
+function displayFinish () {
+    displayResult();
+}
+
+//increment score
+function incrementScore() {
+    const selectedAnswer = $($('input:checked')[0]).val();
+    const rightAnswer = STORE[questionNumber].correctAnswer;
+    if (selectedAnswer == rightAnswer) {
+        console.log(true);
+        correctAnswer();
+    } else {
+        console.log(false);
+        wrongAnswer();
+    }
+    console.log(selectedAnswer);
+    console.log(rightAnswer);
+}
+
+// right answer score
+function correctAnswer() {
+    score++;
+    $('.score').text(score);
+    $('.quiz-container').css('display', 'none');
+    $('.quiz-feedback-correct').css('display', 'flex');
+    $('.correct-img').html(`<img src = ${STORE[questionNumber].icon} alt=${STORE[questionNumber].alt} />`)
+}
+
+// //wrong answer score
+function wrongAnswer() {
+    $('.score').text(score);
+    $('.quiz-container').css('display', 'none');
+    $('.quiz-feedback-wrong').css('display', 'flex');
+    const correctAnswerPosition = STORE[questionNumber].correctAnswer;
+    $('.right-answer').html(`The right answer is: ${STORE[questionNumber].answers[correctAnswerPosition]}`)
+    $('.wrong-img').html(`<img src = ${STORE[questionNumber].icon} alt=${STORE[questionNumber].alt} />`)
+}
+
+//display results >7 == great job; 5-7 == you're on your way; <5 == brush up a little bit by visiting
+function displayResult() {
+    if (score >= 7) {
+        $('.quiz-container').html(`<div class="quiz-result"><p>You got ${score}/${STORE.length}</p><h2>You're a true Austinite!</h2> <img src="https://flygv.com/wp-content/uploads/2018/05/Austin-TX.jpg" alt="Image of Austin, TX and the Texas flag" /> <button class="restart">Restart Quiz</button></div>`);
+    } else if (score >= 5 && score < 7) {
+        $('.quiz-container').html(`<div class="quiz-result"><p>You got ${score}/${STORE.length}</p><h2>You know a lot about Austin!</h2> <img src="https://localadventurer.com/wp-content/uploads/2017/06/greetings-from-austin-tx-mural.jpg" alt="Mural of Austin, TX" /> <button class="restart">Restart Quiz</button></div>`);
+    } else if (score < 5) {
+        $('.quiz-container').html(`<div class="quiz-result"><p>You got ${score}/${STORE.length}</p><h2>Come visit to learn more about Austin!</h2> <img src="https://medialibrarycdn.entrata.com/media_library/4682/Places-to-visit-in_1513629750.jpg" alt="Image of Austin skyline with the text Austin, TX" /> <button class="restart">Restart Quiz</button></div>`);
+    }
+}
+
+//restart the quiz
+function restartQuiz() {
+    $('body').on('click', '.restart', (event) => {
+        questionNumber = 0;
+        score = 0;
+        $('.questionNumber').text(1);
+        $('.score').text(0);
+        $('.quiz-result').html(``);
+        $('.score-container').css("display", "none");
+        $('#intro').css("display", "block");
+        $('.quiz-container').css("display", "none");
+        displayQuestion();
+    });
+}
+
+function createQuiz () {
+    startQuiz();
+    displayQuestion();
+    restartQuiz();
+};
+
+$(createQuiz);
+
+//responsive and css
+//push to github
